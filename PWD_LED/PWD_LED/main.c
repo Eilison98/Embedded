baa2fa83197527da6e8602af36408598c5a8ccb1
Eilison98 <in05013300@gmail.com>
2022-06-22 17:35:28 +0900

    2022-06-22

--- PWD_LED/PWD_LED/main.c
+++ PWD_LED/PWD_LED/main.c
@@ -1,18 +1,33 @@
-/*
- * PWD_LED.c
- *
- * Created: 2022-06-22 오후 4:28:17
- * Author : PKNU
- */ 
-
+#define F_CPU 7432800UL
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	unsigned char Light = 0;
+	
+	// 포트B 를 출력포트로 설정
+	DDRB = 0x10;
+	
+	// 타이머/카운터 제어 레지스터(TCCRn)
+	// PC PWM 모드, 1024 분주 ==> 14.4 KHz :
+	TCCR0 = 0x77;
+	
+	// 타이머/카운터 레지스터(TCNTn)
+	// 타이머0 카운터를 초기화
+	TCNT0 = 0x00;
+	
+	while (1)
+	{
+		for(Light=0;Light<255;Light++)
+		{
+			OCR0 = Light;
+			_delay_ms(10);
+		}
+		for(Light=255;0<Light;Light--)
+		{
+			OCR0 = Light;
+			_delay_ms(10);
+		}
+	}
 }
-
